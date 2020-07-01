d3ec64a98f8d80bdaf7a976b0292a566ae16aed7
Benhur <benhur.bittenca@gmail.com>
2020-07-01 15:57:53 -0300

    executavel

---
+++ exe.py
@@ -0,0 +1,6 @@
+import subprocess
+
+activate_this = r'C:\Users\benhur.bittencourt\Envs\WebScrapy\Scripts\python'
+script_file = r"C:\Users\benhur.bittencourt\Envs\WebScrapy\roboCPFL_Chrome.py"
+
+subprocess.Popen([activate_this, script_file])
